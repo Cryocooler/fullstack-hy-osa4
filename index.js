94c64dba70c78a9f90c11c86becbc4ed053921c3
Sahin Dursun <sahin.dursun@relexsolutions.com>
2022-01-15 11:14:50 +0200

    Factoring application

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-require('dotenv').config()
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Blog = require('./models/blog')
-app.use(cors())
-app.use(express.json())
+require('dotenv').config();
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const app = express();
+const Blog = require('./models/blog');
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+    .then((blogs) => {
+      response.json(blogs);
+    });
+});
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog(request.body);
 
   blog
     .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+    .then((result) => {
+      response.status(201).json(result);
+    });
+});
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
--- models/blog.js
+++ models/blog.js
@@ -1,33 +1,31 @@
-const mongoose = require('mongoose')
-//const validator = require('mongoose-unique-validator')
+const mongoose = require('mongoose');
+const config = require('../utils/config');
+// const validator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
+console.log('connecting to', config.MONGODB_URI);
 
-mongoose.connect(url)
-  .then(res => {console.log('Connected to MongoDB')
-    })
-  .catch(error => {
-    console.log('Error connecting to MongoDB')
-    })
-
+mongoose.connect(config.MONGODB_URI)
+  .then((res) => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB');
+  });
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-    })
-      
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
 
 blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-        }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 
-})
+});
 
-
-module.exports = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model('Blog', blogSchema);
